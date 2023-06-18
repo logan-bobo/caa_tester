f6df0f06a044f291dac828d5b4d7042f8f710f33
Logan Cox <mail@logan-cox.com>
2023-06-18 20:41:48 +0100

    integrate css and node modules with go route

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-	"fmt"
 
 	"net/http"
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "hello\n")
+func home(w http.ResponseWriter, req *http.Request) {
+	http.ServeFile(w, req, "templates/index.html")
 }
 
 func main() {
-	http.HandleFunc("/", hello)
+	http.HandleFunc("/", home)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
+	http.Handle("/node_modules/", http.StripPrefix("/node_modules/", http.FileServer(http.Dir("node_modules"))))
+	
 
 	http.ListenAndServe(":8090", nil)
 }
